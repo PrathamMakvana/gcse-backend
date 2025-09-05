88b2eb1938a313c7fbaeffa580f10ef208d09a9e
mkc110891 <mkc110891@gmail.com>
2025-09-04 21:02:19 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const ALLOW_ORIGINS = [
   'https://gcse-admin-panel.vercel.app',
   'https://gcseadmin.tutoh.ai',
   'https://node-gcse-backend-157527989777.europe-west2.run.app',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'http://34.95.123.156:80'
 ];
 
 // ===== CORS Setup =====
